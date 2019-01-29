5f510cf597114ff158f85a2884e22f2528775a00
James Sear <jpsear@gmail.com>
2019-01-29 11:06:18 +0000

    Re-architects the schema folder, enabling better support for other GQL schemas

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server')
-const { TYPE_DEFINITION } = require('./schema')
+const schema = require('./schema')
 const { queryTypes, resolvers } = require('./queries')
 
 const server = new ApolloServer({
   typeDefs: [
     queryTypes,
-    TYPE_DEFINITION
+    schema
   ],
   resolvers
 })
